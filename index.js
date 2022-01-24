b70738b7a17e72354c30167173e94b6b55bd2a9a
Jon-Collin Chinje <j.chinje@gmail.com>
2022-01-24 11:15:01 -0500

    initial setup

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-test
+fetch('https://zoo-animal-api.herokuapp.com/animals/rand/10')
+    .then (r => r.json())
+    .then (animalObject => {
+        console.log(animalObject)
+        animalObject.forEach(animal => renderAnimal(animal))
+        
+    });
+
